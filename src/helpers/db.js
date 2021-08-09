4fd22d59bb4e19f45ebdb7f845b0b872062db07d
Rikirhenaldi <riki.rhenaldi24@gmail.com>
2021-08-09 14:12:42 +0700

    deploy backend coffeeshop

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,8 +1,7 @@
 const mysql = require('mysql2')
-const { DB_HOST, DB_NAME, DB_USER, DB_PASS, PORT } = process.env
+const { DB_HOST, DB_NAME, DB_USER, DB_PASS } = process.env
 const options = {
   host: DB_HOST,
-  port: PORT,
   database: DB_NAME,
   user: DB_USER,
   password: DB_PASS
